ff1e2db8a64634b7be04fd7f50d435f6182aeaa7
FACTORYS2MBR <dev@s2mbr.com.br>
2023-11-18 19:07:32 -0300

    Project nq-connect-server created

---
+++ nq-connect-server/src/test/java/com/s2mbr/nqconnectserver/NqConnectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2mbr.nqconnectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NqConnectServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
